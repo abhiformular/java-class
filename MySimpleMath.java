133fc5e68c91e27e0b2fa57d1ab9f507b1fa11e3
abhiformular <54816545+abhiformular@users.noreply.github.com>
2020-07-06 19:36:44 -0700

    Add files via upload
    java homework 1

---
+++ MySimpleMath.java
@@ -0,0 +1,14 @@
+
+public class MySimpleMath {
+
+	public static void main(String[] args) {
+		
+		int x = 20 ;
+		int y = 30 ;
+		int z = x + y ;
+		System.out.println(z);
+		int a = x * y ;
+		System.out.println(a);
+	}
+
+}
