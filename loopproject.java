de88714b1aee3a205ee1edd6c57c0e9c152d551d
abhiformular <54816545+abhiformular@users.noreply.github.com>
2020-07-08 17:14:06 -0700

    Add files via upload

---
+++ loopproject.java
@@ -0,0 +1,17 @@
+package loophomework;
+
+public class loopproject {
+
+	public static void main(String[] args) {
+		/// TODO Auto-generated method stub
+		int x = 1;
+		int tablenumber = 6;
+		for (; x <= 10; x = x + 1) {
+			// Multiply
+			System.out.println(tablenumber + " * " + x + " = " + x * tablenumber);
+		//	System.out.println(tablenumber * x);
+
+		}
+
+	}
+}
