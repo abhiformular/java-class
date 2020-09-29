acbdc90d6ef8b5fd8113141042e3f3422dc19b11
abhiformular <54816545+abhiformular@users.noreply.github.com>
2020-09-28 17:59:19 -0700

    Add files via upload

---
+++ javaTest/q3.java
@@ -0,0 +1,15 @@
+package q3;
+
+public class q3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		TV myTV = new TV();
+		System.out.println("The TV's color is " + myTV.color);
+		
+		System.out.println("The TV's size is " + myTV.size);
+		
+		System.out.println("The TV's price is $" + myTV.price);
+	}
+}
